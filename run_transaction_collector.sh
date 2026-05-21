f8b4bfc05eeea400c21946cfb22eb78c215733ed
Orange Bitman <110528110+OrangeBitman@users.noreply.github.com>
2026-05-21 08:59:34 +0200

    add new arguments for new functionality logging, split-combined mode etc

--- run_transaction_collector.sh
+++ run_transaction_collector.sh
@@ -1,1 +1,1 @@
-python3 transaction_collector.py --csv-out 1_a1ef55cc86c0596.csv --mempool-base https://mempool.blocktrainer.de/api a1ef55cc86c0596b55ca92360715a57071cb3d598571013b51121eed4d2642c7
+python3 transaction_collector.py resources/transactions.txt --decimal-sep , --log-file transaction_collector.log --mempool-base https://mempool.space/api --overwrite --output-mode split --digits 3
